03dc85ee245ec969873f2c53f863608f3505dd80
Alexander Bender (né Toenges) <21218078+ATpoint@users.noreply.github.com>
2023-04-13 12:38:31 +0200

    Update validate_samplesheet.R

--- bin/validate_samplesheet.R
+++ bin/validate_samplesheet.R
@@ -1,6 +1,7 @@
 args = commandArgs(trailingOnly=TRUE)
 ss_file <-args[1]
 ss <- read.delim(ss_file, sep=",", header=TRUE)
+ss[is.na(ss)] <- ""
 
 l_error <- list()
 
@@ -15,19 +16,15 @@ for(u in unique_sample){
   # Libtype for same sample must be identical
   sx <- ss[ss$sample==u,]
   
-  #sx_group <- unique(sx$group)
-  #if(length(sx_group)>1)
-  #  l_error[paste0("group_", u)] <- paste("Sample:", u, "has multiple different groups")
-  
   # R1 and R2 must not have duplicates
   sx_r1 <- table(sx$r1)
-  sx_r1w <- which(sx_r1>01)
+  sx_r1w <- which(sx_r1>1)
   if(length(sx_r1w)>0) 
     l_error[paste0("r1_", u)] <- paste("Duplicated r1:", paste(names(sx_r1)[sx_r1w], collapse = ","))
   
   if(unique(sx$r2)[1]!=""){
     sx_r2 <- table(sx$r2)
-    sx_r2w <- which(sx_r2>01)
+    sx_r2w <- which(sx_r2>1)
     if(length(sx_r2w)>0) 
       l_error[paste0("r2_", u)] <- paste("Duplicated r2:", paste(names(sx_r2)[sx_r2w], collapse = ","))
   }
@@ -37,7 +34,7 @@ for(u in unique_sample){
     l_error[paste0("notsame_", u)] <- paste("Sample:", u, "has a mix of SE and PE files")
   
   rm(list=c(ls(pattern="^sx")))
-    
+  
 }
 
 if(length(l_error)>0){
